6d827d85b5fe4a619d36c2aee69942546a7b2a34
Benjamin Schultzer <benjamin@schultzer.com>
2016-11-28 21:19:09 -0800

    Removed unused variable

--- controllers/api_receipt.js
+++ controllers/api_receipt.js
@@ -4,7 +4,6 @@ module.exports = function () {
   var multer = require('multer')
   var crypto = require('crypto')
   var mime = require('mime')
-  var fs = require('fs')
   var scanner = require('receipt-scanner')
 
   var storage = multer.diskStorage({
